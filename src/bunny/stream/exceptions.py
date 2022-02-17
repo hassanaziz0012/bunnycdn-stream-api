603d4580cb45ce7c93b2e64ed053d8edf61a3adc
Hassan <hassanaziz0012@gmail.com>
2022-02-17 21:33:02 +0500

    Clear cache

--- src/bunny/stream/exceptions.py
+++
@@ -1,8 +0,0 @@
-class AuthorizationError(Exception):
-    def __init__(self, message) -> None:
-        super().__init__(message)
-
-
-class ServerError(Exception):
-    def __init__(self, message) -> None:
-        super().__init__(message)
